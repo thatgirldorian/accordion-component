af4d9c3126a086be950f78cb1829f078dacbecf0
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-07-09 14:53:08 +0400

    Added custom copy to the FAQ page page

---
+++ app.js
@@ -0,0 +1,14 @@
+//Open and close all the accordion items with a ForEach loop
+const accordions = Array.from(document.querySelectorAll('.accordion'))
+
+accordions.forEach(accordion => {
+    //find the accordion header 
+    const accordionHeader = accordion.querySelector('.accordion__header')
+
+    //add event listener to the header
+    accordionHeader.addEventListener('click', event => {
+         //toggle the is-open class
+            accordion.classList.toggle('is-open')
+    })
+
+})
